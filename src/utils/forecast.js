9917045b0842852ba175e1d1d9d7c311476a661c
unknown <mos.gom.17@gmail.com>
2025-04-19 10:39:57 +0200

    Add new Feature

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,7 +1,4 @@
-
 const request = require('request')
-
-
 const forecast = (latitude, longitude, callback) => {
     const url = 'https://api.weatherstack.com/current?access_key=fcf973f86eb0a6555a7e5cc1bffc92eb&query=' + latitude + ',' + longitude + '&units=f'
 
@@ -11,14 +8,10 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location. Try another search.', undefined)
         } else {
-            //callback(undefined, response.body.current.temperature)
-            callback(undefined, body.current.weather_descriptions[0] + ' It is currently ' + body.current.temperature + ' degress out. There is a ' + body.current.weather_code + ' Done!')
+            callback(undefined, body.current.weather_descriptions[0] + ' It is currently ' + body.current.temperature + ' degress out. It feels like ' + body.current.feelslike + ' degress out. The humidity is ' + body.current.humidity + '%')
         }
     })
-
 }
-
-
 module.exports = forecast
 
 
